9812bff5de9bfaa3f7786727abbb46ab4aa46cdc
Eason  Liang <eliang@uoguelph.ca>
2024-02-19 19:22:49 -0500

    Implement Login page with Supabase and Navigation

--- App.js
+++ App.js
@@ -1,11 +1,180 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-url-polyfill/auto";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import { supabase } from "./lib/supabase";
+import Auth from "./components/Auth";
+import Home from "./pages/Home";
+import Cookbook from "./pages/Cookbook";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
+import GroceryList from "./pages/GroceryList";
 
 export default function App() {
+  const [session, setSession] = useState(null);
+  const [activeItem, setActiveItem] = useState(null);
+  
+  useEffect(() => {
+    const fetchSession = async () => {
+      const { data: session, error } = await supabase.auth.getSession();
+      if (error) {
+        console.error("Error fetching session:", error.message);
+      } else {
+        setSession(session);
+      }
+    };
+
+    fetchSession();
+
+    const authListener = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+
+  const handlePress = (itemName, props) => {
+    setActiveItem(itemName);
+    props.navigation.navigate(itemName);
+  };
+  function CustomDrawerContent(props) {
+    return (
+      <DrawerContentScrollView
+        {...props}
+        contentContainerStyle={{ paddingTop: 0 }}
+      >
+        <View
+          style={{
+            height: 150,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#D75B3F",
+            paddingTop: 0,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 20,
+              backgroundColor: "#D75B3F",
+              paddingTop: "10%",
+            }}
+          >
+            RecipeShare
+          </Text>
+        </View>
+        <DrawerItem
+          label="My Recipes"
+          onPress={() => handlePress("My Recipe", props)}
+          activeBackgroundColor="#D9D9D9"
+          inactiveBackgroundColor="white"
+          inactiveTintColor="#000000"
+          activeTintColor="#C0452A"
+          focused={activeItem === "My Recipe"}
+          style={{ borderBottomWidth: 1, borderBottomColor: '#AEAEAE', paddingBottom:10}}
+
+        />
+      <Text style={{ borderBottomWidth: 1, borderBottomColor: '#AEAEAE' }}></Text>
+        <DrawerItem
+          label="Cookbooks"
+          onPress={() => handlePress("Cookbooks", props)}
+          activeBackgroundColor="#D9D9D9"
+          inactiveBackgroundColor="white"
+          inactiveTintColor="#000000"
+          activeTintColor="#C0452A"
+          focused={activeItem === "Cookbooks"}
+        />
+        <DrawerItem
+          label="Grocery List"
+          onPress={() => handlePress("Grocery List", props)}
+          activeBackgroundColor="#D9D9D9"
+          inactiveBackgroundColor="white"
+          inactiveTintColor="#000000"
+          activeTintColor="#C0452A"
+          focused={activeItem === "Grocery List"}
+          style={{ borderBottomWidth: 1, borderBottomColor: '#AEAEAE', paddingBottom:10}}
+        />
+        <DrawerItem
+          label="Account"
+          onPress={() => handlePress("Account", props)}
+          activeBackgroundColor="#D9D9D9"
+          inactiveBackgroundColor="white"
+          inactiveTintColor="#000000"
+          activeTintColor="#C0452A"
+          focused={activeItem === "Account"}
+        />
+        <DrawerItem
+          label="Settings"
+          onPress={() => handlePress("Settings", props)}
+          activeBackgroundColor="#D9D9D9"
+          inactiveBackgroundColor="white"
+          inactiveTintColor="#000000"
+          activeTintColor="#C0452A"
+          focused={activeItem === "Settings"}
+          style={{ borderBottomWidth: 1, borderBottomColor: '#AEAEAE', paddingBottom:10}}
+        />
+        <DrawerItem
+          label="Sign Out"
+          onPress={() => signOut()}
+          activeBackgroundColor="#D9D9D9"
+          inactiveBackgroundColor="white"
+          inactiveTintColor="#000000"
+          activeTintColor="#C0452A"
+          focused={activeItem === "Sign Out"}
+        />
+      </DrawerContentScrollView>
+    );
+  }
+
+  const Drawer = createDrawerNavigator();
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) console.error("Error signing out:", error.message);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {session && session.user ? (
+        <NavigationContainer>
+          <Drawer.Navigator
+            initialRouteName="Home"
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+          >
+            <Drawer.Screen
+              name="My Recipe"
+              component={Home}
+              options={styles.options}
+            />
+            <Drawer.Screen
+              name="Cookbooks"
+              component={Cookbook}
+              options={styles.options}
+            />
+            <Drawer.Screen
+              name="Grocery List"
+              component={GroceryList}
+              options={styles.options}
+            />
+            <Drawer.Screen
+              name="Account"
+              component={Profile}
+              options={styles.options}
+            />
+            <Drawer.Screen
+              name="Settings"
+              component={Settings}
+              options={styles.options}
+            />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      ) : (
+        <Auth />
+      )}
     </View>
   );
 }
@@ -13,8 +182,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  options: {
+    unmountOnBlur: true,
+    drawerActiveBackgroundColor: "#D9D9D9",
+    drawerInactiveBackgroundColor: "white",
+    drawerActiveTintColor: "#C0452A",
+    drawerInactiveTintColor: "#000000",
   },
 });
---
+++ pages/GroceryList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const GroceryList = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>GroceryList</Text>
+    </View>
+  );
+}
+
+export default GroceryList;
