4dbc19d16c995bea0b2ee4b490f9fdf250daa1b8
Eason  Liang <eliang@uoguelph.ca>
2024-02-24 22:39:46 -0500

    Add the add cookbook modal with upload image and everything :)

---
+++ Navigation.js
@@ -0,0 +1,198 @@
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import CommunityMaterialIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
+import { supabase } from "./lib/supabase";
+import Auth from "./components/Auth";
+import Home from "./pages/Home";
+import Cookbook from "./pages/Cookbook";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
+import GroceryList from "./pages/GroceryList";
+
+export default function Navigation() {
+  const [session, setSession] = useState(null);
+  const [activeItem, setActiveItem] = useState(null);
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      const { data: session, error } = await supabase.auth.getSession();
+      error
+        ? console.error("Error fetching session:", error.message)
+        : setSession(session);
+    };
+
+    fetchSession();
+
+    const authListener = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) console.error("Error signing out:", error.message);
+  };
+
+  const handlePress = (itemName, props) => {
+    if (itemName !== "Sign Out") {
+      setActiveItem(itemName);
+      props.navigation.navigate(itemName);
+      return;
+    }
+    signOut();
+  };
+
+  const icon = (type, color, size, name) => {
+    return type === "Community" ? (
+      <CommunityMaterialIcon name={name} size={size} color={color} />
+    ) : (
+      <MaterialIcon name={name} size={size} color={color} />
+    );
+  };
+
+  const renderItem = (label, itemName, type, props, iconName) => (
+    <DrawerItem
+      label={label}
+      onPress={() => handlePress(itemName, props)}
+      activeBackgroundColor="#D9D9D9"
+      inactiveBackgroundColor="white"
+      inactiveTintColor="#000000"
+      activeTintColor="#C0452A"
+      focused={activeItem === itemName}
+      style={{
+        borderBottomWidth: 1,
+        borderBottomColor: "#AEAEAE",
+        paddingBottom: 10,
+      }}
+      icon={() => icon(type, "black", 20, iconName)}
+    />
+  );
+
+  function CustomDrawerContent(props) {
+    return (
+      <DrawerContentScrollView
+        {...props}
+        contentContainerStyle={{ paddingTop: 0 }}
+      >
+        <View
+          style={{
+            height: 150,
+            alignItems: "center",
+            justifyContent: "flex-end",
+            backgroundColor: "#D75B3F",
+            paddingTop: 0,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 40,
+              color: "#FFFFFF",
+              paddingBottom: "5%",
+              fontFamily: "Purtian",
+            }}
+          >
+            RecipeShare
+          </Text>
+        </View>
+        {renderItem(
+          "My Recipes",
+          "My Recipe",
+          "Community",
+          props,
+          "silverware"
+        )}
+        {renderItem(
+          "Favourited Recipes",
+          "Favourited Recipies",
+          "Community",
+          props,
+          "cards-heart"
+        )}
+        {renderItem(
+          "Cookbooks",
+          "Cookbooks",
+          "Community",
+          props,
+          "book-open-page-variant-outline"
+        )}
+        {renderItem(
+          "Grocery List",
+          "Grocery List",
+          "Community",
+          props,
+          "cart-outline"
+        )}
+        {renderItem("Account", "Account", "", props, "account-circle")}
+        {renderItem("Settings", "Settings", "", props, "settings")}
+        {renderItem("Sign Out", "Sign Out", "Community", props, "logout")}
+      </DrawerContentScrollView>
+    );
+  }
+
+  const Drawer = createDrawerNavigator();
+
+  return (
+    <View style={styles.container}>
+      {session && session.user ? (
+        <NavigationContainer>
+          <Drawer.Navigator
+            initialRouteName="Home"
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+          >
+            <Drawer.Screen
+              name="My Recipe"
+              component={Home}
+              options={styles.options}
+            />
+            <Drawer.Screen
+              name="Cookbooks"
+              component={Cookbook}
+              options={styles.options}
+            />
+            <Drawer.Screen
+              name="Grocery List"
+              component={GroceryList}
+              options={styles.options}
+            />
+            <Drawer.Screen
+              name="Account"
+              component={Profile}
+              options={styles.options}
+            />
+            <Drawer.Screen
+              name="Settings"
+              component={Settings}
+              options={styles.options}
+            />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      ) : (
+        <Auth />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  options: {
+    unmountOnBlur: true,
+    drawerActiveBackgroundColor: "#D9D9D9",
+    drawerInactiveBackgroundColor: "white",
+    drawerActiveTintColor: "#C0452A",
+    drawerInactiveTintColor: "#000000",
+    headerTitleAlign: "center",
+    headerStyle: {
+      backgroundColor: "#A7CCA2", // Set the background color of the header for this screen
+    },
+  },
+});
---
+++ components/CookbookModal.js
@@ -0,0 +1,168 @@
+// CookbookModal.js
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Image,
+  StyleSheet,
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import UploadIcon from "react-native-vector-icons/Feather";
+import * as ImagePicker from "expo-image-picker";
+
+const CookbookModal = ({ modalVisible, setModalVisible }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      setSelectedImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={styles.centeredView}>
+      <View style={styles.modalView}>
+        <TouchableOpacity
+          style={styles.closeIcon}
+          onPress={() => {
+            setModalVisible(false);
+            setSelectedImage(null);
+          }}
+        >
+          <Icon name="close" size={30} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.modalText}>Create Cookbook</Text>
+        <View style={styles.cookbookTitleContainer}>
+          <Text style={styles.titleModalText}>Cookbook title</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="My Cookbook Title"
+            placeholderTextColor="#888"
+            inputMode="text"
+            keyboardType="default"
+          />
+        </View>
+        <TouchableOpacity style={styles.container} onPress={pickImage}>
+          <View style={styles.frame}>
+            {selectedImage ? (
+              <Image source={{ uri: selectedImage }} style={styles.image} />
+            ) : (
+              <View style={styles.placeholder}>
+                <Text style={styles.imageText}>
+                  Upload a Cover Image {"\n\n"}
+                  <UploadIcon name="upload" size={30} color="black" />
+                </Text>
+              </View>
+            )}
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.createButton}>
+          <Text style={{ color: "white", fontSize: 20 }}>Create</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalView: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    width: "80%",
+    maxWidth: 500,
+    height: 450,
+    alignSelf: "center",
+  },
+  closeIcon: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    paddingBottom: 5,
+  },
+  cookbookTitleContainer: {
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    width: "100%",
+    paddingBottom: 20,
+    paddingTop: 20,
+  },
+  frame: {
+    width: 150,
+    height: 150,
+    borderWidth: 2,
+    borderColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 0,
+    marginBottom: 60,
+  },
+  input: {
+    marginTop: 10,
+    height: 40,
+    borderColor: "transparent",
+    borderBottomColor: "#888",
+    borderBottomWidth: 1,
+    width: "100%",
+    fontSize: 15,
+    color: "#333",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    borderRadius: 6,
+  },
+  placeholder: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#EAEAEA",
+    borderRadius: 6,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modalText: {
+    fontSize: 22,
+    fontFamily: "Puritan",
+  },
+  titleModalText: {
+    paddingTop: 10,
+    fontSize: 18,
+    fontFamily: "Puritan",
+    marginTop: 10,
+  },
+  createButton: {
+    backgroundColor: "#D75B3F",
+    padding: 20,
+    paddingLeft: 40,
+    paddingRight: 40,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 50,
+    alignItems: "center",
+    position: "absolute",
+    bottom: 10,
+  },
+  imageText: {
+    textAlign: "center",
+    fontSize: 15,
+  },
+});
+
+export default CookbookModal;
