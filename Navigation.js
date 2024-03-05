7015a19319afb652078babe54b14d69ed4979119
BrittanyMueller <brittany_mueller@sympatico.ca>
2024-03-05 01:07:42 -0500

    Refactored navigation to use Stacks and custom drawer components.

--- Navigation.js
+++ Navigation.js
@@ -1,14 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItem,
-} from "@react-navigation/drawer";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import CommunityMaterialIcon from "react-native-vector-icons/MaterialCommunityIcons";
-import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 import { supabase } from "./lib/supabase";
 import Auth from "./components/Auth";
 import RecipeHome from "./pages/RecipeHome";
@@ -17,129 +11,33 @@ import FavouriteRecipes from "./pages/FavouriteRecipes";
 import CookbookHome from "./pages/CookbookHome";
 import GroceryList from "./pages/GroceryList";
 import Settings from "./pages/Settings";
-import Profile from "./pages/Profile";
+import Account from "./pages/Account";
 import Toolbar from "./components/Toolbar";
 import RecipeToolbar from "./components/RecipeToolbar";
 
-export default function Navigation() {
-  const [session, setSession] = useState(null);
-  const [activeItem, setActiveItem] = useState(null);
+import CustomDrawer from "./components/CustomDrawer";
 
+export default function Navigation() {
+  
+  const [session, setSession] = useState(null);
+  
   useEffect(() => {
+    // TODO want to review this, why are the functions defined with the use effect?
+    // Should be dependent on "session" state no?
     const fetchSession = async () => {
       const { data: session, error } = await supabase.auth.getSession();
       error
-        ? console.error("Error fetching session:", error.message)
-        : setSession(session);
+      ? console.error("Error fetching session:", error.message)
+      : setSession(session);
     };
-
     fetchSession();
-
     const authListener = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
     });
+    
   }, []);
 
-  const signOut = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) console.error("Error signing out:", error.message);
-  };
-
-  const handlePress = (itemName, props) => {
-    if (itemName !== "Sign Out") {
-      setActiveItem(itemName);
-      props.navigation.navigate(itemName);
-      return;
-    }
-    signOut();
-  };
-
-  const icon = (type, color, size, name) => {
-    return type === "Community" ? (
-      <CommunityMaterialIcon name={name} size={size} color={color} />
-    ) : (
-      <MaterialIcon name={name} size={size} color={color} />
-    );
-  };
-
-  const renderItem = (label, itemName, type, props, iconName) => (
-    <DrawerItem
-      label={label}
-      onPress={() => handlePress(itemName, props)}
-      activeBackgroundColor="#D9D9D9"
-      inactiveBackgroundColor="white"
-      inactiveTintColor="#000000"
-      activeTintColor="#C0452A"
-      focused={activeItem === itemName}
-      style={{
-        borderBottomWidth: 1,
-        borderBottomColor: "#AEAEAE",
-        paddingBottom: 10,
-      }}
-      icon={() => icon(type, "black", 20, iconName)}
-    />
-  );
-
-  function CustomDrawerContent(props) {
-    return (
-      <DrawerContentScrollView
-        {...props}
-        contentContainerStyle={{ paddingTop: 0 }}
-      >
-        <View
-          style={{
-            height: 150,
-            alignItems: "center",
-            justifyContent: "flex-end",
-            backgroundColor: "#D75B3F",
-            paddingTop: 0,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 40,
-              color: "#FFFFFF",
-              paddingBottom: "5%",
-            }}
-          >
-            RecipeShare
-          </Text>
-        </View>
-        {renderItem(
-          "My Recipes",
-          "RecipeHome",
-          "Community",
-          props,
-          "silverware"
-        )}
-        {renderItem(
-          "Favourited Recipes",
-          "FavouriteRecipes",
-          "Community",
-          props,
-          "cards-heart"
-        )}
-        {renderItem(
-          "Cookbooks",
-          "CookbookHome",
-          "Community",
-          props,
-          "book-open-page-variant-outline"
-        )}
-        {renderItem(
-          "Grocery List",
-          "Grocery List",
-          "Community",
-          props,
-          "cart-outline"
-        )}
-        {renderItem("Account", "Account", "", props, "account-circle")}
-        {renderItem("Settings", "Settings", "", props, "settings")}
-        {renderItem("Sign Out", "Sign Out", "Community", props, "logout")}
-      </DrawerContentScrollView>
-    );
-  }
-
+  const Drawer = createDrawerNavigator();
   const Stack = createNativeStackNavigator();
 
   const RecipeStack = () => {
@@ -159,14 +57,13 @@ export default function Navigation() {
     );
   };
 
-  const Drawer = createDrawerNavigator();
-
   return (
-    <View style={styles.container}>
+    <View style={styles.root}>
       {session && session.user ? (
         <NavigationContainer>
           <Drawer.Navigator
-            drawerContent={(props) => <CustomDrawerContent {...props} />}
+            initialRouteName="RecipeHome"
+            drawerContent={({navigation}) => <CustomDrawer navigation={navigation} />}
           >
             <Drawer.Screen
               name="RecipeStack"
@@ -184,13 +81,13 @@ export default function Navigation() {
               options={{ header: () => <Toolbar title={"Cookbooks"} /> }}
             />
             <Drawer.Screen
-              name="Grocery List"
+              name="GroceryList"
               component={GroceryList}
               options={{ header: () => <Toolbar title={"Grocery List"} /> }}
             />
             <Drawer.Screen
               name="Account"
-              component={Profile}
+              component={Account}
               options={{ header: () => <Toolbar title={"Account"} showSearch={false} /> }}
             />
             <Drawer.Screen
@@ -208,18 +105,7 @@ export default function Navigation() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-  },
-  options: {
-    unmountOnBlur: true,
-    drawerActiveBackgroundColor: "#D9D9D9",
-    drawerInactiveBackgroundColor: "white",
-    drawerActiveTintColor: "#C0452A",
-    drawerInactiveTintColor: "#000000",
-    headerTitleAlign: "center",
-    headerStyle: {
-      backgroundColor: "#A7CCA2", // Set the background color of the header for this screen
-    },
-  },
+  }
 });
---
+++ components/CustomDrawer.js
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import { supabase } from "../lib/supabase";
+import { Text, View, TouchableOpacity, StyleSheet} from 'react-native'
+import CommunityMaterialIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const CustomDrawer = ({ navigation }) => {
+
+  const [activeScreen, setActiveScreen] = useState('RecipeStack');
+
+  const handleDrawerClick = (screenName) => {
+    navigation.navigate(screenName);
+    setActiveScreen(screenName);
+  }
+  
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) console.error("Error signing out:", error.message);
+  };
+
+  const CustomDrawerItem = ({ label, screenName, isActive, icon }) => {
+    return (
+      <TouchableOpacity 
+        style={[styles.drawerItem, isActive && styles.activeItem]} 
+        onPress={() => handleDrawerClick(screenName)}
+        activeOpacity={0.8}
+      >
+        {React.cloneElement(icon, {color: isActive ? '#C0452A' : 'black'})}
+        <Text style={[styles.drawerText, isActive && styles.activeText]}>{label}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <View styles={styles.drawer}>
+      <View style={styles.topBar}>
+        <Text style={styles.title}>RecipeShare</Text>
+      </View>
+      <CustomDrawerItem 
+        label={"My Recipes"} 
+        screenName={'RecipeStack'} 
+        isActive={activeScreen === 'RecipeStack'}
+        icon={<CommunityMaterialIcon color="black" size={24} name="silverware"/>}
+      />
+      {/* TODO add the horizontal lines between subsections? */}
+      {/* <View style={{ marginTop: 2, backgroundColor: 'black', height: 1 }} /> */}
+      <CustomDrawerItem 
+        label={"Favourited Recipes"} 
+        screenName={'FavouriteRecipes'} 
+        isActive={activeScreen === 'FavouriteRecipes'}
+        icon={<FontAwesomeIcon color="black" size={24} name="heart"/>}
+      />
+      <CustomDrawerItem 
+        label={"Cookbooks"} 
+        screenName={'CookbookHome'} 
+        isActive={activeScreen === 'CookbookHome'}
+        icon={<CommunityMaterialIcon color="black" size={24} name="book-open-page-variant-outline"/>}
+      />
+      <CustomDrawerItem 
+        label={"Grocery List"} 
+        screenName={'GroceryList'} 
+        isActive={activeScreen === 'GroceryList'}
+        icon={<CommunityMaterialIcon color="black" size={24} name="cart-outline"/>}
+      />
+      <CustomDrawerItem 
+        label={"Account"} 
+        screenName={'Account'} 
+        isActive={activeScreen === 'Account'}
+        icon={<Icon color="black" size={24} name="person-circle-outline"/>}
+      />
+      <CustomDrawerItem 
+        label={"Settings"} 
+        screenName={'Settings'} 
+        isActive={activeScreen === 'Settings'}
+        icon={<CommunityMaterialIcon color="black" size={24} name="cog"/>}
+      />
+      <TouchableOpacity 
+        style={styles.drawerItem} 
+        onPress={() => signOut()}
+        activeOpacity={0.8}
+      >
+        <FontAwesomeIcon color="black" size={24} name="sign-out"/>
+        <Text style={styles.drawerText}>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  drawer: {
+    width: "60%"
+  },
+  topBar: {
+    marginTop: 25,  // TODO remove this once fix status bar spacing
+    height: 100,
+    backgroundColor: "#D75B3F",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "white",
+    letterSpacing: 2,
+    fontSize: 28
+  },
+  drawerText: {
+    color: "black",
+    fontSize: 18
+  },
+  drawerItem: {
+    height: 60,
+    marginTop: 5,
+    marginBottom: 0,
+    paddingLeft: 20,  
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20 // Gap between icon & label
+  },
+  activeItem: {
+    backgroundColor: "#E7E7E7",
+  },
+  activeText: {
+    color: '#C0452A'
+  }
+})
+
+export default CustomDrawer;
