652406731944ceba66601e9448e3be511906a91a
Eason  Liang <eliang@uoguelph.ca>
2024-04-05 04:16:04 -0400

    Add usecallback import

--- pages/CookbookPage.js
+++ pages/CookbookPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useCallback } from "react";
 import { supabase } from '../lib/supabase'
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import {
